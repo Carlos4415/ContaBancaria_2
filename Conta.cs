6c6b1f5931d96a295091dfcdb5ad5ac7bc176b57
Carlos4415 <carlos.soares4415@gmail.com>
2025-02-05 18:17:36 -0300

    first commit

---
+++ Conta.cs
@@ -0,0 +1,9 @@
+﻿class Conta
+{
+    public Titular Titular { get; set; }
+    public int Agencia { get; set; }
+    public int NumeroConta { get; set; }
+    public double Saldo { get; set; }
+    public double Limite { get; set; }
+    public string Informações => $"Conta nº {this.NumeroConta}, Agência {this.Agencia}, Titular: {this.Titular.Nome} - Saldo: {this.Saldo}";
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Classe Conta que representa uma conta bancária, com atributos Titular, agência, número da conta, saldo e limite.  
+// Inclui um método que retorna as informações detalhadas da conta.  
+Titular t = new();
+Conta c = new();
+
+t.Nome = "George Harrison";
+t.Cpf = "000.000.000-00";
+t.Endereco = "Rua dos Besouros - Liverpool";
+
+c.Titular = t;
+c.Agencia = 1;
+c.NumeroConta = 2234;
+c.Saldo = 10000000.0;
+c.Limite = 100000.0;
+
+Console.WriteLine("Informações do Titular: ");
+Console.WriteLine($"Nome: {c.Titular.Nome}");
+Console.WriteLine($"CPF: {c.Titular.Cpf}");
+Console.WriteLine($"Endereço: {c.Titular.Endereco}");
+
+Console.WriteLine("\nInformações da conta");
+Console.WriteLine(c.Informacoes);
+Console.WriteLine($"Limite: {c.Limite}");
